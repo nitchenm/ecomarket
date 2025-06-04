6cd62fab1d7ac2719565450e005759a0ff4cf8c2
Domingo Velazquez <domingoandresv98@gmail.com>
2025-06-04 19:44:24 -0400

    Se hicieron pequeñas correciones al product microservice

--- microservice-product/src/main/java/com/microservice_product/MicroserviceProductApplication.java
+++ microservice-product/src/main/java/com/microservice_product/MicroserviceProductApplication.java
@@ -2,7 +2,9 @@ package com.microservice_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceProductApplication {
 
