d3d66fef21c7fc4f1272da3c44b08539dddee8d2
nit <nitchenm@gmail.com>
2025-06-14 12:36:58 -0400

    Se añade Dataloader y dependencias anexas

---
+++ microservice-sale/src/main/java/com/acopl/microservice_sale/DataLoader.java
@@ -0,0 +1,36 @@
+package com.acopl.microservice_sale;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.acopl.microservice_sale.model.Sale;
+import com.acopl.microservice_sale.repository.ISaleRepository;
+
+import net.datafaker.Faker;
+
+@Profile("test")
+@Component
+public class DataLoader implements CommandLineRunner{
+    @Autowired
+    private ISaleRepository saleRepository;
+
+    @Override 
+    public void run(String... args) throws Exception{
+        Faker faker = new Faker();
+        Random random = new Random();
+        
+        for (Long i = (long) 0; i< 3 ; i++){
+            Sale sale = new Sale();
+            sale.setId(i+1);
+            sale.setClientId(i +1);
+            sale.setProductId(i+1);
+            sale.setTotal(i+200);
+            sale.setDateTime(new Date());
+        }
+    }
+}
