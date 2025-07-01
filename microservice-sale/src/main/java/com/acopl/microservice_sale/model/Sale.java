6732d4c2dda4fda8596e226ea6533f20c51f06a9
nitchenm <nitchenm@gmail.com>
2025-06-30 22:30:57 -0400

    Se completa configuracion de swagger y se añade saleControllerV2

--- microservice-sale/src/main/java/com/acopl/microservice_sale/model/Sale.java
+++ microservice-sale/src/main/java/com/acopl/microservice_sale/model/Sale.java
@@ -1,14 +1,12 @@
 package com.acopl.microservice_sale.model;
 
 import java.util.Date;
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,19 +19,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Sale {
-
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-
     private Date dateTime;
-
     private float total;
-
     private Long clientId;
-
     private Long productId;
-
-
-
 }
